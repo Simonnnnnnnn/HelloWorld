2802b9c342fa703cd55a71d3b7e5117bf87ab378
xiang.zhang <xiang.zhang@infocore.cn>
2020-02-15 14:54:33 +0800

    TestExport

---
+++ TestExportJar/src/com/infocore/demo/TestExport.java
@@ -0,0 +1,14 @@
+package com.infocore.demo;
+
+/**
+ * @author XiangZhang
+ * @date 2020/2/15 - 14:44
+ */
+public class TestExport {
+
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+
+
+}
